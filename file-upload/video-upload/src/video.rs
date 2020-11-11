6b4d75e57992ceced2a2c362e2d921eed48970a4
shenshing <lyshenshing20@gmail.com>
2020-11-11 21:52:11 +0700

    file-upload

---
+++ file-upload/video-upload/src/video.rs
@@ -0,0 +1,41 @@
+use std::io::Write;
+
+use actix_multipart::Multipart;
+use actix_web::{web, Error, HttpResponse};
+use futures::{StreamExt, TryStreamExt};
+
+pub fn index() -> HttpResponse {
+    let html = r#"<html>
+        <head><title>Upload Test</title></head>
+        <body>
+            <form target="/" method="post" enctype="multipart/form-data">
+                <input type="file" multiple name="file"/>
+                <button type="submit">Submit</button>
+            </form>
+        </body>
+    </html>"#;
+
+    HttpResponse::Ok().body(html)
+}
+
+pub async fn save_file(mut payload: Multipart) -> Result<HttpResponse, Error> {
+    // iterate over multipart stream
+    while let Ok(Some(mut field)) = payload.try_next().await {
+        let content_type = field.content_disposition().unwrap();
+        let filename = content_type.get_filename().unwrap();
+        let filepath = format!("./tmp/{}", sanitize_filename::sanitize(&filename));
+
+        // File::create is blocking operation, use threadpool
+        let mut f = web::block(|| std::fs::File::create(filepath))
+            .await
+            .unwrap();
+
+        // Field in turn is stream of *Bytes* object
+        while let Some(chunk) = field.next().await {
+            let data = chunk.unwrap();
+            // filesystem operations are blocking, we have to use threadpool
+            f = web::block(move || f.write_all(&data).map(|_| f)).await?;
+        }
+    }
+    Ok(HttpResponse::Ok().into())
+}
---
+++ run/src/main.rs
@@ -0,0 +1,23 @@
+use actix_web::{HttpServer, App, web, middleware};
+
+use video_upload::video::{save_file, index};
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_server=info,actix_web=info");
+    std::fs::create_dir_all("./tmp").unwrap();
+
+    let ip = "0.0.0.0:3000";
+
+    HttpServer::new(|| {
+        App::new().wrap(middleware::Logger::default()).service(
+            web::resource("/")
+                .route(web::get().to(index))
+                .route(web::post().to(save_file)),
+        )
+    })
+    .bind(ip)?
+    .run()
+    .await
+}
